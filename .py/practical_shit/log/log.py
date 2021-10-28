94e2cc7244fbe4ee9ce35f225b97899b4c823571
ISC4T <si1744@skole.trondheim.kommune.no>
2021-10-28 17:39:26 +0200

    master

--- .py/practical_shit/log/log.py
+++ .py/practical_shit/log/log.py
@@ -1,33 +1,49 @@
+import time
+
+userLogin = False
 signOrSetup = input("Sign in, Sign up: ")
 
-def signIn():
+def signIn(): # Sign in
+    user = input("Username? ")
+    userPass = (input("Password? "))
+
+    with open('userDB.txt') as file: # Checks the login credentials
+     if ((user + "\n") + userPass) in file.read():
+         userLogin = True
+
+def signUp(): # Sign Up
     user = input("Username? ")
     userPass = (input("Password? "))
 
     with open('userDB.txt') as file:
-     if (user + userPass) in file.read():
-         userLogin = True
-
-def signUp():
-    user = input("Username? ")
-    userPass = (input("Password? "))
-    with open("userDB.txt", "r") as file:
-        userDB_file = file.readlines()
+        if (user) in file.read(): # Checks if Username exists
+            print("Username Already exists")
+        else:
+            with open("userDB.txt", "r") as file: # Pusts (Username + Password) in userDB.txt
+                userDB_file = file.readlines()
         
-    userDB_file.append((user + userPass) + "\n")
+            userDB_file.append(((user + "\n") + userPass) + "\n")
 
-    with open("userDB.txt", "a") as userDB:
-	    contents = "".join(userDB_file)
-	    userDB.write(contents)
+            with open("userDB.txt", "a") as userDB:
+	            contents = "".join(userDB_file)
+	            userDB.write(contents)
 
-    loopsignin = input("Sign In(Y, N)? ")
-    if loopsignin.lower() == ("y"):
-        signIn()
+            loopsignin = input("Sign In(Y, N)? ") # Asks if you want to sign in
+            if loopsignin.lower() == ("y"):
+                signIn()
 
+# Start and Confirm Sign up
 if signOrSetup.lower() == ("sign up"):
     signUp()
 
+# Start and Confirm Sign in
 if signOrSetup.lower() == ("sign in"):
     signIn()
     if userLogin == True:
          print("Success!")
+    else:
+        print("Login Failed")
+        print("Try Again")
+        time.sleep(1)
+        signIn()
+       
