4ba81e2bc9ef7b0a9b2822cab79bfbdfa0055f65
Sigurd Osen <sigsos1@hotmail.com>
2022-05-31 14:30:01 +0200

    turtle

---
+++ .py/practical_shit/turtleFotballbane.py/bane.py
@@ -0,0 +1,16 @@
+import turtle
+import math
+
+tr = turtle.screen
+tr.title("Fifa gokjent bane")
+tr.bgcolor("green")
+tr.setup(width=111, height=76, startx=3, starty=3) #True size 105, 68
+tr.speed(0)
+tr.forward(105)
+tr.left
+tr.forward(68)
+tr.left
+tr.forward(105)
+tr.left
+tr.forward(68)
+turtle.done
